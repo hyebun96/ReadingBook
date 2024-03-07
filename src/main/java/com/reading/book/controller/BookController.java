9ab93a1a395ec3a1faecebe5a906b418c573a745
hyebun <hyebun96@naver.com>
2024-03-07 23:07:42 +0900

    feat: 도서 검색 리스트 기능 구현
    - 도서 검색 기능 구현
    - 도서 검색 리스트에서 필수값인 도서 이미지와 이름만 가져오는 domain생성

--- src/main/java/com/reading/book/controller/BookController.java
+++ src/main/java/com/reading/book/controller/BookController.java
@@ -1,15 +1,45 @@
 package com.reading.book.controller;
 
+import com.reading.api.NaverBookAPI;
+import com.reading.api.domain.Book;
+import com.reading.api.domain.NaverResultVO;
+import com.reading.book.domain.BookListResponseDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/book")
+@RequiredArgsConstructor
+@Log4j2
 public class BookController {
 
+    private final NaverBookAPI naverBookAPI;
+
+    private final ModelMapper modelMapper;
+
     @GetMapping("/search")
-    public String search(){
+    public String search(@RequestParam("title") String title, Model model) throws IOException {
+
+        NaverResultVO naverResultVO = naverBookAPI.searchBookAll(title);
+
+        List<Book> items = naverResultVO.getItems();
+        List<BookListResponseDTO> searchBookList = items.stream()
+                .map(book -> modelMapper.map(book, BookListResponseDTO.class))
+                .toList();
+
+        model.addAttribute("searchBookList", searchBookList);
+        model.addAttribute("title", title);
+
         return "/book/search";
     }
+
 }
---
+++ src/main/java/com/reading/book/domain/BookListResponseDTO.java
@@ -0,0 +1,6 @@
+package com.reading.book.domain;
+
+public class BookListResponseDTO {
+    private String image;
+    private String title;
+}
