a5b8014b3b44ecfeecd47cd648c193a317648c5d
hyebun <hyebun96@naver.com>
2024-03-27 23:38:36 +0900

    feat: pageRequestDTO @Builder 추가

--- src/main/java/com/reading/book/dto/PageRequestDTO.java
+++ src/main/java/com/reading/book/dto/PageRequestDTO.java
@@ -1,14 +1,21 @@
 package com.reading.book.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@Builder
 public class PageRequestDTO {
 
-    private int total = 0;   // 결과값 페이지 수
-    private int start = 1;   // 시작 값
-    private int display = 10; // 한 페이지 당 보여줄 결과물.
+    private int total;       // 결과값 페이지 수
+    private int start;       // 시작 값
+    private int display;     // 한 페이지 당 보여줄 결과물.
 
+    @Builder
+    public PageRequestDTO() {
+        this.total = 0;
+        this.start = 1;
+        this.display = 10;
+    }
 }
