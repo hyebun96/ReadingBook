e7b2262ccff1505ca8c1f10b1ea732c32df8bf3a
jhye-hwa <jhyehwa0@gmail.com>
2024-04-23 01:18:32 +0900

    refactor: 메인에 차트관련 js 에러 수정
    [index.html]
    - dashboards-analytics.js 에서 chart.js 을 호출해서 사용해야하는데 chart.js 뒤에 dashboards-analytics.js 을 호출해야 해서 layout을 수정
    
    [script.html]
    - font-awesome / bootstrap 링크 → header.html 로 이동
    
    [header.html]
    - script.html에 선언되어 있던 font-awesome / bootstrap 링크 추가
    
    [layout.html]
    - dashboards-analytics.js 에서 chart.js 을 호출해서 사용해야하는데 chart.js 뒤에 dashboards-analytics.js 을 호출해야 해서 layout을 수정
    
    [ReadingBookRepositorySupport.java]
    - monthDataList 역순으로 정렬하기 위해 수정

--- src/main/java/com/reading/readingBook/repository/ReadingBookRepositorySupport.java
+++ src/main/java/com/reading/readingBook/repository/ReadingBookRepositorySupport.java
@@ -6,7 +6,9 @@ import com.reading.readingBook.dto.MonthChartResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
@@ -19,22 +21,24 @@ public class ReadingBookRepositorySupport {
 
     private final JPAQueryFactory jpaQueryFactory;
 
-    public Map<String, Object> findAllByMonthChartData(Long id) throws Exception {
+    public Map<String, Object> findAllByMonthChartData(Long id) {
         LocalDateTime now = LocalDateTime.now();
 
         List<MonthChartResponseDto> monthDataList = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            LocalDateTime dateTime = now.minusMonths(i);
-            LocalDateTime from = dateTime.withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0);
-            LocalDateTime to = from.withDayOfMonth(from.getMonth().maxLength()).withHour(23).withMinute(59).withSecond(59);
 
-            MonthChartResponseDto dto = Optional.ofNullable(jpaQueryFactory.select(
-                                    Projections.constructor(MonthChartResponseDto.class,
-                                            bookReport.count()))
+        for (int i = 0; i < 12; i++) {
+            LocalDate dateTime = now.toLocalDate().minusMonths(i);
+            LocalDate from = dateTime.withDayOfMonth(1);
+            LocalDate to = from.withDayOfMonth(from.lengthOfMonth());
+
+            LocalDateTime fromDateTime = LocalDateTime.of(from, LocalTime.MIN);
+            LocalDateTime toDateTime = LocalDateTime.of(to, LocalTime.MAX);
+
+            MonthChartResponseDto dto = Optional.ofNullable(jpaQueryFactory
+                            .select(Projections.constructor(MonthChartResponseDto.class, bookReport.count()))
                             .from(bookReport)
-                            .innerJoin(bookShelf)
-                            .on(bookReport.id.eq(bookShelf.bookReport.id))
-                            .where(bookReport.regDate.between(from, to), bookShelf.member.id.eq(id)) // memberId랑 book_report_id 조건에 넣어야함 bookShelf.member.id.eq(id)
+                            .innerJoin(bookShelf).on(bookReport.id.eq(bookShelf.bookReport.id))
+                            .where(bookReport.regDate.between(fromDateTime, toDateTime), bookShelf.member.id.eq(id))
                             .groupBy(bookReport.regDate.yearMonth())
                             .fetchOne())
                     .orElseGet(MonthChartResponseDto::new);
@@ -42,6 +46,9 @@ public class ReadingBookRepositorySupport {
             dto.setMonth(from.format(DateTimeFormatter.ofPattern("yyyy-MM")));
             monthDataList.add(dto);
         }
+
+        // monthDataList 역순으로 정렬
+        Collections.reverse(monthDataList);
 
         Map<String, Object> data = new HashMap<>();
         data.put("monthData", monthDataList);
