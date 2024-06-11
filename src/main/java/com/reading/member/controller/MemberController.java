5f1d437a227522b6b7aaa4681971dcc971b19055
hyebun <hyebun96@naver.com>
2024-06-11 17:01:46 +0900

    feat: member 컨트롤러 CORS 적용
    - 배포 https 접속을 위한 member 컨트롤러 CORS 적용

--- src/main/java/com/reading/member/controller/MemberController.java
+++ src/main/java/com/reading/member/controller/MemberController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.io.IOException;
 import java.util.Map;
 
+@CrossOrigin(origins = "https://port-0-intobooks-1lx8gjwrw.sel5.cloudtype.app/")
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
